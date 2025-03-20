2c9baef2c0313d3880df606204a41cd4f1299e63
JirapatDeesara <jirapat.dees@bumail.net>
2025-03-20 10:43:15 +0700

    Add Magnus Effect
    - Add Magnus script

---
+++ Assets/Scripts/MagnusEffect.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class MagnusEffect : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] Vector3 velocity, spin;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();    
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // kick the ball like Roberto Carlos
+            Kick();
+        //apply magnus effect
+        ApplyMagnusEffect();
+        }
+    }
+
+    void Kick()
+    {
+        rb.linearVelocity = velocity; // kick forward
+        rb.angularVelocity = spin; // kick spin Roberto carlos style
+    }//End Method Kick    
+
+    void ApplyMagnusEffect()
+    {
+        Vector3 magnusForce = Vector3.Cross(rb.linearVelocity , rb.angularVelocity);
+        rb.AddForce(magnusForce, ForceMode.Force);   
+    }// end Method ApplyMagnusEffect
+}// End MagnusEffect
